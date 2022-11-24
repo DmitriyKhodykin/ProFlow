df6bb90a94c770d3a1d424171f0ef46b878f974a
Dmitry.Khodykin <dmitry.hodykin@itentika.ru>
2022-11-24 18:46:53 +0300

    init

--- proflow/data/data_loader.py
+++ proflow/data/data_loader.py
@@ -1,10 +1,10 @@
+"""Data load module."""
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 import yaml
 from yaml.loader import SafeLoader
-
-
 
 
 class DataLoader:
@@ -14,18 +14,35 @@ class DataLoader:
 
     def data_load(self, df_dir: str, label: str):
 
-        with open('data_config.yaml') as _config:
+        with open('proflow/data/data_config.yaml') as _config:
             params = yaml.load(_config, Loader=SafeLoader)
-            train_size = params["data"]["preparation"]["train_size"]
+            test_partition = params["data"]["preparation"]["test_size"]
+            seed = params["seed"]
 
         df = pd.read_csv("../../datasets/pochta_rf/train_dataset.csv")
-        x_train, x_test, y_train, y_test = train_test_split(
-            X, 
-            y, 
-            test_size=1-train_size, 
-            random_state=42
+        
+        train_df, temp_df = train_test_split(
+            df.drop([label], axis=1), 
+            df[label],
+            test_size=test_partition, 
+            random_state=seed,
         )
-        return x_train, x_test, y_train, y_test
+        
+        test_df, valid_df = train_test_split(
+            temp_df.drop([label], axis=1),
+            temp_df[label],
+            test_size=50,
+            random_state=seed,
+        )
+        
+        print(train_df.shape, test_df.shape, valid_df.shape)
+
+        return train_df, test_df, valid_df
 
     def data_types_detector(self):
         pass
+
+
+if __name__=="__main__":
+    c = DataLoader()
+    c.data_load("", "label")
