bf73a19a9bf0a66a88d7ac11b4612a6d69a8f82d
Dmitry.Khodykin <dmitry.hodykin@itentika.ru>
2022-11-24 18:09:16 +0300

    init

---
+++ proflow/automl/tabular_tasks.py
@@ -0,0 +1,21 @@
+"""AutoML Module."""
+
+class AutoML:
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def print_score(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def predict_proba(self):
+        pass
+
+    def save_best_model(self):
+        pass
