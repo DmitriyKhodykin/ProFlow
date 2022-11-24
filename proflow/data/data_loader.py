75e6c0d97ad0a3ca80691b0a5a454f26ef3a0f47
Dmitry.Khodykin <dmitry.hodykin@itentika.ru>
2022-11-24 22:18:58 +0300

    init

--- proflow/data/data_loader.py
+++ proflow/data/data_loader.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from prettytable import PrettyTable
 
 import yaml
 from yaml.loader import SafeLoader
@@ -12,30 +13,35 @@ class DataLoader:
     def __init__(self):
         pass
 
-    def data_load(self, df_dir: str, label: str):
+    def data_load(self, df_dir: str):
 
         with open('proflow/data/data_config.yaml') as _config:
             params = yaml.load(_config, Loader=SafeLoader)
             test_partition = params["data"]["preparation"]["test_size"]
             seed = params["seed"]
 
-        df = pd.read_csv("../../datasets/pochta_rf/train_dataset.csv")
+        df = pd.read_csv(df_dir)
         
         train_df, temp_df = train_test_split(
-            df.drop([label], axis=1), 
-            df[label],
+            df,
             test_size=test_partition, 
             random_state=seed,
+            shuffle=True,
         )
         
         test_df, valid_df = train_test_split(
-            temp_df.drop([label], axis=1),
-            temp_df[label],
-            test_size=50,
+            temp_df,
+            test_size=0.5,
             random_state=seed,
+            shuffle=True,
         )
-        
-        print(train_df.shape, test_df.shape, valid_df.shape)
+
+        shape_table = PrettyTable()
+        shape_table.field_names = ["Partition", "[0]_shape", "[1]_shape"]
+        shape_table.add_row(["train_df", train_df.shape[0], train_df.shape[1]])
+        shape_table.add_row(["test_df", test_df.shape[0], test_df.shape[1]])
+        shape_table.add_row(["valid_df", valid_df.shape[0], valid_df.shape[1]])
+        print(shape_table)
 
         return train_df, test_df, valid_df
 
@@ -44,5 +50,5 @@ class DataLoader:
 
 
 if __name__=="__main__":
-    c = DataLoader()
-    c.data_load("", "label")
+    loader = DataLoader()
+    loader.data_load("../../datasets/pochta_rf/train_dataset.csv", "label")
