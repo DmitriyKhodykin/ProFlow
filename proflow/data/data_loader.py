00096488f8ec61edc9cd56a37333c482e67fd69d
Dmitry.Khodykin <dmitry.hodykin@itentika.ru>
2022-12-04 18:41:04 +0300

    init

--- proflow/data/data_loader.py
+++ proflow/data/data_loader.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from prettytable import PrettyTable
 
-from data_imputer import Imputer
+from proflow.data.data_imputer import Imputer
 from proflow import config
 
 
@@ -22,9 +22,9 @@ class DataLoader:
     ):
         df = pd.read_csv(df_dir, dtype={"index_oper": "str"}).sample(500_000)
 
-        df = df[["priority", "weight", "label"]]
+        df = df[["index_oper" ,"priority", "weight", "label"]]
         
-        train_df, temp_df = train_test_split(
+        train_df, test_df = train_test_split(
             df,
             test_size=self.test_partition, 
             random_state=self.seed,
@@ -36,18 +36,26 @@ class DataLoader:
 
         shape_table.field_names = ["Partition", "[0]_shape", "[1]_shape"]
         shape_table.add_row(["train_df", train_df.shape[0], train_df.shape[1]])
-        shape_table.add_row(["test_df", temp_df.shape[0], temp_df.shape[1]])
+        shape_table.add_row(["test_df", test_df.shape[0], test_df.shape[1]])
 
         if data_imputer == True:
-            train_df_imputed = impputer.imput_data(train_df)
-            temp_df_imputed = impputer.imput_data(temp_df)
+            train_df_imputed = impputer.imput_data(train_df.drop([label], axis=1))
+            test_df_imputed = impputer.imput_data(test_df.drop([label], axis=1))
+
+            train_df_imputed = pd.DataFrame(train_df_imputed).assign(label = train_df[label].values)
+            test_df_imputed = pd.DataFrame(test_df_imputed).assign(label = test_df[label].values)
+            
+            shape_table.add_row(["train_df_imputed", train_df.shape[0], train_df.shape[1]])
+            shape_table.add_row(["test_df_imputed", test_df.shape[0], test_df.shape[1]])
+
             print(shape_table)
             print(train_df_imputed.head())
-            return train_df_imputed, temp_df_imputed
+            return train_df_imputed, test_df_imputed
         
-        print(shape_table)
-        print(train_df.head())
-        return train_df, temp_df
+        else:
+            print(shape_table)
+            print(train_df.head())
+            return train_df, test_df
 
     def _data_types_detector(self):
         pass
